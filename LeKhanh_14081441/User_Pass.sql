fbfedda6e747a1c0d1641df1b60b9c9187717a68
ungdungdoan <ungdungdoan@gmail.com>
2016-09-19 07:41:11 +0700

    Add files via upload
    database login

---
+++ LeKhanh_14081441/User_Pass.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE ADVANCED
+-- Create table
+create table USER_ACCOUNT
+(
+USER_NAME VARCHAR(30) not null,
+GENDER    VARCHAR(1) not null,
+PASSWORD  VARCHAR(30) not null,
+primary key (USER_NAME)
+);
+ 
+-- Create table
+create table SUBJECT
+(
+CODE  VARCHAR(20) not null,
+NAME  VARCHAR(128) not null,
+PRICE FLOAT not null,
+primary key (CODE)
+) ;
+ 
+-- Insert data: ---------------------------------------------------------------
+ 
+insert into user_account (USER_NAME, GENDER, PASSWORD)
+values ('tom', 'M', 'tom001');
+ 
+insert into user_account (USER_NAME, GENDER, PASSWORD)
+values ('jerry', 'M', 'jerry001');
+ 
+insert into subject (CODE, NAME, PRICE)
+values ('P001', 'Java Core', 100);
+ 
+insert into subject (CODE, NAME, PRICE)
+values ('P002', 'C# Core', 90);
